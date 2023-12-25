8d601b7d479666399cff387ba2614f9adf1b6c2a
originalmahim <tareqazizmahim@gmail.com>
2023-12-25 12:36:01 +0600

    Created business section successfully

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
-import Billing from "./Hero/Billing";
-import Hero from "./Hero/Hero";
-import Stats from "./Hero/Stats";
+import Billing from "./Sections/Billing";
+import Business from "./Sections/Business";
+import Hero from "./Sections/Hero";
+import Stats from "./Sections/Stats";
 
 const Home = () => {
           return (
           <div className="max-w-7xl mx-auto">
            <Hero></Hero>
            <Stats></Stats>
+           <Business></Business>
            <Billing></Billing>                   
           </div>
           );
