754875005c59217cfef3dc4d8de2a510bdb0c776
originalmahim <tareqazizmahim@gmail.com>
2023-12-25 12:09:08 +0600

    Created Stats Section Successfully

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Billing from "./Hero/Billing";
 import Hero from "./Hero/Hero";
+import Stats from "./Hero/Stats";
 
 const Home = () => {
           return (
           <div className="max-w-7xl mx-auto">
            <Hero></Hero>
+           <Stats></Stats>
            <Billing></Billing>                   
           </div>
           );
